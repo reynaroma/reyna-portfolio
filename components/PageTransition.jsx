4379015aa57a9753990461160b930b3ddc1f60c2
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 17:36:55 -0600

    Import motion and usePathname

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 function PageTransition({ children }) {
   return (
