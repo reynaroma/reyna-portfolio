902cc6b6db443e2fe68465a5f86bcea37e32c47e
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 17:00:37 -0600

    Add links object and import the Link and usePathname

--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -1,8 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation"; // to get the current path
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 function Nav() {
   return (
     <nav>
-      desktop nav
+
     </nav>
   )
 }
