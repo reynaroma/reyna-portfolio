89f2aba917fcf4f21a05e0e84326479a7897e994
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-19 21:06:38 -0600

    Add style on the Header logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,14 @@ import { Button } from "./ui/button";
 function Header() {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">header</div>
+      <div className="container mx-auto">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Reyna<span className="text-accent">.</span>
+          </h1>
+        </Link>
+      </div>
     </header>
   )
 }
