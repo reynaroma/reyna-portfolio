7635070553b6d4c4614860327a5c64a3e045f9ed
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-19 21:02:38 -0600

    Import the Button from ui and Link from next

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 function Header() {
   return (
-    <header>
-      header
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">header</div>
     </header>
   )
 }
