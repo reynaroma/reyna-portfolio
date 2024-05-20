0179f071245e4a4e8ef676026aa8d8d44ed0c74b
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 16:56:14 -0600

    Add flexbox on the div navs

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ import Nav from "./ui/Nav";
 function Header() {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between">
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
@@ -16,7 +16,7 @@ function Header() {
         </Link>
 
         {/* desktop nav & hire me button*/}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire me</Button>
