5feb3308845f6a0e0514647f9672cb2c54bbac62
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 20:52:45 -0600

    Add div element for the download cv

--- app/page.jsx
+++ app/page.jsx
@@ -15,6 +15,13 @@ const Home = () => {
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
             </p>
+            {/* btn and socials */}
+            <div>
+              <Button>
+                <span>Download CV</span>
+                <FiDownload />
+              </Button>
+            </div>
           </div>
           {/* photo */}
           <div>photo</div>
