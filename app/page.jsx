4284a7978c192cdb5f91e9ea2f065580bb90355e
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 22:00:39 -0600

    Add styles on the Social

--- app/page.jsx
+++ app/page.jsx
@@ -28,7 +28,9 @@ const Home = () => {
                 <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social />
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="flex w-9 h-9 border border-accent rounded-full justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
