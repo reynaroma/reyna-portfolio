f8ab71d5082c6ea41a5d04f8674c208bc3d2fda2
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 18:24:28 -0600

    Add comment for description of the function

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -15,9 +15,11 @@ const stairAnimation = {
 
 function Stairs() {
   return (
-    <div>
-      Stairs
-    </div>
+    <>
+
+      {/* render 6 motion divs, each representiong a step of the stairs.  Each will have the same animation defined by the stairsAnimation object. The delay for each div is calculated dynamically based on it's reversed index, creating a staggered effect with decreasing delay for each subsequent step.*/}
+
+    </>
   )
 }
 
