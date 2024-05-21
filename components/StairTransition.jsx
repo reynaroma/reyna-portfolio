c9a9cf36d9b576e950fbb80dc18bbc273dda8eb5
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 18:17:00 -0600

    Add style on the  stair div

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -4,10 +4,18 @@ import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 function StairTransition() {
+  const pathName = usePathname();
+
   return (
-    <AnimatePresence>
-      Stair Transition
-    </AnimatePresence>
+    <>
+      <AnimatePresence mode="wait">
+        <div key={pathName}>
+          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+            stairs
+          </div>
+        </div>
+      </AnimatePresence>
+    </>
   )
 }
 
