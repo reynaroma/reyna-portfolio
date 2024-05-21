d8a9c61a0611c284ca03f727a5fbfaf2ce17ff52
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 18:21:53 -0600

    Add new variants object on stairs component

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,8 +1,22 @@
+import { motion } from "framer-motion";
+
+// variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
 
 function Stairs() {
   return (
     <div>
-      
+      Stairs
     </div>
   )
 }
