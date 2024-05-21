2b2f75dd9d2f4e8ae4d1f5cdba81ff45330282de
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 19:42:10 -0600

    Change the default style of x icon

--- components/ui/sheet.jsx
+++ components/ui/sheet.jsx
@@ -51,7 +51,7 @@ const SheetContent = React.forwardRef(({ side = "right", className, children, ..
     <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
       {children}
       <SheetPrimitive.Close
-        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800">
+        className="absolute right-8 top-8 transition-opacity outline-none">
         <IoMdClose className="text-3xl text-accent" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
