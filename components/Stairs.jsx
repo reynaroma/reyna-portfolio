37e1e6180616e8636d0888f1a7da060877050571
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-20 18:37:30 -0600

    Add an array for 6 elements then style and  animate it

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -24,7 +24,21 @@ function Stairs() {
     <>
 
       {/* render 6 motion divs, each representiong a step of the stairs.  Each will have the same animation defined by the stairsAnimation object. The delay for each div is calculated dynamically based on it's reversed index, creating a staggered effect with decreasing delay for each subsequent step.*/}
-
+      {[...Array(6)].map((_, index) => (
+        <motion.div
+          key={index}
+          variants={stairAnimation}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: reverseIndex(index) * 0.1,
+          }}
+          className="h-full w-full bg-white relative"
+        />
+      ))}
     </>
   )
 }
