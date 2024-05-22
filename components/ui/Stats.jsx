302cd647502afc83d836a39c4486fc70860411e6
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-21 23:03:43 -0600

    Add paragraph on the countup element

--- components/ui/Stats.jsx
+++ components/ui/Stats.jsx
@@ -8,11 +8,11 @@ const stats = [
     text: "Years of Experience",
   },
   {
-    num: 12,
+    num: 26,
     text: "Lorem ipsum",
   },
   {
-    num: 12,
+    num: 8,
     text: "Blah blah",
   },
   {
@@ -25,15 +25,17 @@ const Stats = () => {
   return (
     <section>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((stat, index) => (
             <div
+              className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
               key={index}>
               <CountUp
                 end={stat.num}
                 duration={5}
                 delay={2}
                 className="text-4xl xl:text-6xl font-extrabold" />
+              <p className={`${stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"}`}>{stat.text}</p>
             </div>
           ))}
         </div>
