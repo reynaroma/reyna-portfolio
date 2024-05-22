ebc626afaaea452cd9816d27d91d6000389f17c9
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-21 21:13:11 -0600

    Add circle animation on the photo

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -25,9 +25,23 @@ const Photo = () => {
         </motion.div>
 
         {/* Circle */}
-        {/* <motion.svg className="w-[300px]">
-          <motion.circle />
-        </motion.svg> */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[400px] xl:h-[506px]" fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="https://www.w3.org/200/svg"
+        >
+          <motion.circle
+            cx="253" cy="253"
+            r="250" stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16, 25, 92, 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }} />
+        </motion.svg>
       </motion.div>
     </div>
   )
