cc41dc8fa13e6214f3d7bff8c2253463eac2bd8c
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-21 20:12:20 -0600

    Add the image to the photo component

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div>Photo</div>
+    <div className="w-full h-full relative">
+      <motion.div>
+        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
+          <Image
+            src="/photo.png" priority quality={100} fill alt="" className="object-contain" />
+        </div>
+      </motion.div>
+    </div>
   )
 }
 
