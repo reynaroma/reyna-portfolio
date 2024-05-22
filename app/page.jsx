ce1606b8fa117724f8abe56d8104cd7e2da3f93a
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-21 20:36:46 -0600

    Addorder when in mobile devices

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I&lsquo;m  <br /> <span className="text-accent">Reyna May Roma</span>
@@ -36,7 +36,7 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
