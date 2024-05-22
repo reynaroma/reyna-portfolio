bc8462a6ec9ae4154430e1cf892902955e4588d1
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-21 22:46:19 -0600

    Add the CountUp package and use it

--- components/ui/Stats.jsx
+++ components/ui/Stats.jsx
@@ -1,7 +1,44 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+  {
+    num: 12,
+    text: "Years of Experience",
+  },
+  {
+    num: 12,
+    text: "Lorem ipsum",
+  },
+  {
+    num: 12,
+    text: "Blah blah",
+  },
+  {
+    num: 500,
+    text: "Code commits",
+  },
+]
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section>
+      <div className="container mx-auto">
+        <div>
+          {stats.map((stat, index) => (
+            <div
+              key={index}>
+              <CountUp
+                end={stat.num}
+                duration={5}
+                delay={2}
+                className="text-4xl xl:text-6xl font-extrabold" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
