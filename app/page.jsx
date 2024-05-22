1d54b92df6af937d1cf168816bd381c3a0b31bf4
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-21 20:04:33 -0600

    Add a new component for the photos and add photos

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 // components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -35,7 +36,9 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
