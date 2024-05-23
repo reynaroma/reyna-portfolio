a23b0e0387d656cf84184baabe1ca2f167e65af4
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-22 20:36:56 -0600

    Add class

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -29,10 +29,12 @@ const services = [
   },
 ];
 
-function Services() {
+const Services = () => {
   return (
-    <section className='container mx-auto'>
-      services page
+    <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+      <div className='container mx-auto'>
+        services page
+      </div>
     </section>
   )
 }
