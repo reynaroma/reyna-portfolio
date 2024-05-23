0b7ea62a14d5cf653ea0dd47f46b8a3272e549b7
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-22 19:53:26 -0600

    Add object for services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,33 @@
+"use client";
+
+import { BsArrowBarRight } from 'react-icons/bs';
+
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+  {
+    num: '02',
+    title: 'Web Development',
+    description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+  {
+    num: '03',
+    title: 'Web Development',
+    description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+  {
+    num: '04',
+    title: 'Web Development',
+    description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+];
 
 function Services() {
   return (
