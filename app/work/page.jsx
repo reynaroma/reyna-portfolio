901cf87e047eebbdcb30dd158342c1fe1d8614bc
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-24 17:56:21 -0600

    Add initial setup for slider component with tooltips and icons

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { } from 'framer-motion';
+import React, { useState } from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'; // Import Swiper styles
+
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Work = () => {
   return (
