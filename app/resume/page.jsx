1db503c385da57846dc3777b47f6e30d42a18411
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 19:23:10 -0600

    Add animation on the outer div

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -131,7 +131,24 @@ import { motion } from 'framer-motion';
 
 const Resume = () => {
   return (
-    <div>page</div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={
+        {
+          opacity: 1,
+          transition: {
+            delay: 2.4, duration: 0.4, ease: 'easeIn'
+          }
+        }
+      }
+      className='min-h-[80vh] flex items-center justify-center py-12 xl:py-0'
+    >
+      <div className="container mx-auto">
+        <Tabs>
+          
+        </Tabs>
+      </div>
+    </motion.div>
   )
 }
 
