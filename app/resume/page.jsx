209675730745d68bed0c7a1c7a73536ea4d33fa4
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 23:59:23 -0600

    Add about me outline and style it

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -249,9 +249,9 @@ const Resume = () => {
             </TabsContent>
             {/* about */}
             <TabsContent value="about" className="w-full text-center xl:text-left">
-              <div className='flex flex-col'>
-                <h3>{about.title}</h3>
-                <p>{about.description}</p>
+              <div className='flex flex-col gap-[30px]'>
+                <h3 className='text-4xl font-bold'>{about.title}</h3>
+                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{about.description}</p>
                 <ul>
                   {about.info.map((item, index) => (
                     <li key={index}>
