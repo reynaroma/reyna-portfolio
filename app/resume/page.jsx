5290c0766c527e4478562cf3a2a6a87b308b1ca4
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 23:51:26 -0600

    Add gap on every list

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -229,7 +229,7 @@ const Resume = () => {
                   <h3 className='text-4xl font-bold'>{skills.title}</h3>
                   <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{skills.description}</p>
                 </div>
-                <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]'>
+                <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4'>
                   {skills.skillList.map((skill, index) => (
                     <li key={index}>
                       <TooltipProvider delayDuration={100}>
