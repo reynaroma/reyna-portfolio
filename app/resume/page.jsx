422e4ad6010d4ead3d267670b04a76df4732e019
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 22:11:08 -0600

    Modify the copied version of experience to education

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -91,12 +91,12 @@ const education = {
   items: [
     {
       institution: "Adamson University",
-      degree: "Bachelor of Science in Computer Engineering",
+      degree: "BS in Computer Engineering",
       duration: "2014 - 2020",
     },
     {
       institution: "Lighthouse Labs",
-      degree: "Web Development Bootcamp",
+      degree: "Full Stack Web Development Bootcamp",
       duration: "2023",
     },
   ]
@@ -200,7 +200,27 @@ const Resume = () => {
             </TabsContent>
             {/* education */}
             <TabsContent value="education" className="w-full">
-              education
+              <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                <h3 className='text-4xl font-bold'>{education.title}</h3>
+                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{experience.description}</p>
+                <ScrollArea className='h-[400px]'>
+                  <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
+                    {education.items.map((item, index) => (
+                      <li
+                        className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'
+                        key={index}>
+                        <span className='text-accent'>{item.duration}</span>
+                        <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>{item.degree}</h3>
+                        <div className='flex items-center gap-3'>
+                          {/* dot */}
+                          <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
+                          <p className='text-white/60'>{item.institution}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             {/* skills */}
             <TabsContent value="skills" className="w-full">
