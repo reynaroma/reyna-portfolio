05d9a944b3154d6e6b2a2efef7083623040ad90e
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 21:20:37 -0600

    Add style on the heading and subheading

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -156,7 +156,10 @@ const Resume = () => {
           <div className='min-h-[70vh] w-full'>
             {/* experience */}
             <TabsContent value="experience" className="w-full">
-              experience
+              <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                <h3 className='text-4xl font-bold'>{experience.title}</h3>
+                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{experience.description}</p>
+              </div>
             </TabsContent>
             {/* education */}
             <TabsContent value="education" className="w-full">
