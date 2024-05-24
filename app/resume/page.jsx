f5a19f6cde8c581c29d4a680a59af4b5abb8865f
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 18:17:31 -0600

    Add an object for data info

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,13 +1,52 @@
 "use client";
 
 import { FaThml5, FaCss3, FaReact, FaNodeJs, } from 'react-icons/fa';
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
 import { FaWebflow } from "react-icons/fa6";
 
-function Resume() {
+// about data
+const about = {
+  title: 'About me',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  info: [
+    {
+      fieldName: 'Name',
+      fieldValue: 'Reyna May Roma',
+    },
+    {
+      fieldName: 'Phone',
+      fieldValue: '(+1) 306 684-0292',
+    },
+    {
+      fieldName: 'Experience',
+      fieldValue: '1+ years',
+    },
+    {
+      fieldName: 'Skype',
+      fieldValue: 'Reyna May Roma',
+    },
+    {
+      fieldName: 'Nationality',
+      fieldValue: 'Filipino',
+    },
+    {
+      fieldName: 'Email',
+      fieldValue: 'roma.reynamay@gmail.com',
+    },
+    {
+      fieldName: 'Freelance',
+      fieldValue: 'Available',
+    },
+    {
+      fieldName: 'Language',
+      fieldValue: 'English, Filipino',
+    },
+  ]
+}
+
+const Resume = () => {
   return (
-    <div>
-      resume page
-    </div>
+    <div>page</div>
   )
 }
 
