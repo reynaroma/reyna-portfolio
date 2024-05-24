8c2b44e3b75953d56f1eeba4cf737794f9c6a6a2
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-23 19:16:49 -0600

    Add skills data object

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaThml5, FaCss3, FaReact, FaNodeJs, } from 'react-icons/fa';
+import { FaThml5, FaCss3, FaReact, FaNodeJs, FaJs, } from 'react-icons/fa';
 import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
 import { FaWebflow } from "react-icons/fa6";
 
@@ -42,7 +42,7 @@ const about = {
       fieldValue: 'English, Filipino',
     },
   ]
-}
+};
 
 // experience data
 const experience = {
@@ -61,7 +61,67 @@ const experience = {
       duration: "2023 - 2024",
     },
   ]
-}
+};
+
+// education data
+const education = {
+  icon: '/assets/resume/cap.svg',
+  title: "My education",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  items: [
+    {
+      institution: "Adamson University",
+      degree: "Bachelor of Science in Computer Engineering",
+      duration: "2014 - 2020",
+    },
+    {
+      institution: "Lighthouse Labs",
+      degree: "Web Development Bootcamp",
+      duration: "2023",
+    },
+  ]
+};
+
+// skils data
+const skils = {
+  title: 'My skills',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  skillList: [
+    {
+      icon: <FaThml5 />,
+      name: 'html 5',
+    },
+    {
+      icon: <FaCss3 />,
+      name: 'css 3',
+    },
+    {
+      icon: <FaJs />,
+      name: 'javascript',
+    },
+    {
+      icon: <FaReact />,
+      name: 'react.js',
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: 'next.js',
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: 'tailwind.css',
+    },
+    {
+      icon: <FaNodeJs />,
+      name: 'node.js',
+    },
+    {
+      icon: <FaWebflow />,
+      name: 'Webflow',
+    },
+  ]
+};
+
 
 const Resume = () => {
   return (
