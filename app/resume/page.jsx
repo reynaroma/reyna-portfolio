e7d65cf92f4625a1d60e00547d901ff1bd6bd58f
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-24 00:04:21 -0600

    Style between the fields

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -252,11 +252,13 @@ const Resume = () => {
               <div className='flex flex-col gap-[30px]'>
                 <h3 className='text-4xl font-bold'>{about.title}</h3>
                 <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{about.description}</p>
-                <ul>
+                <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0'>
                   {about.info.map((item, index) => (
-                    <li key={index}>
-                      <span>{item.fieldName}</span>
-                      <span>{item.fieldValue}</span>
+                    <li
+                      className='flex items-center justify-center xl:justify-start gap-4'
+                      key={index}>
+                      <span className='text-white/60'>{item.fieldName}</span>
+                      <span className='text-xl'>{item.fieldValue}</span>
                     </li>
                   ))}
                 </ul>
