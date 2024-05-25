cb16d0f38be60fbe4426c57dedbfd9a079b4a6ce
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-24 20:59:21 -0600

    Add outline for project number

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -69,7 +69,12 @@ const Work = () => {
       className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'>
       <div className="container mx-auto">
         <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
-          <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none'>text</div>
+          <div className='w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none'>
+            <div>
+              {/* outline num */}
+              <div className=''>{projects.num}</div>
+            </div>
+          </div>
           <div className='w-full xl:w-[50%]'>slider</div>
         </div>
       </div>
