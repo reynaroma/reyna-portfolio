ff772d3335ee032db2e4e4dcd46da2ee641a731b
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-24 19:01:20 -0600

    Add data object for works

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -12,6 +12,52 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/comp
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { github } from '@/public/assets';
+
+const projects = [
+  {
+    num: '01',
+    category: 'Frontend',
+    title: 'Project 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+    stack: [
+      { name: 'Html 5' },
+      { name: 'Css 3' },
+      { name: 'JavaScript' },
+    ],
+    image: '/assets/work/thumb1.png',
+    live: '',
+    github: '',
+  },
+  {
+    num: '02',
+    category: 'Frontend',
+    title: 'Project 2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+    stack: [
+      { name: 'Next.js' },
+      { name: 'Tailwind.css 3' },
+      { name: 'Node.js' },
+    ],
+    image: '/assets/work/thumb2.png',
+    live: '',
+    github: '',
+  },
+  {
+    num: '03',
+    category: 'Frontend',
+    title: 'Project 3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+    stack: [
+      { name: 'Next.js' },
+      { name: 'Tailwind.css' },
+      { name: 'JavaScript' },
+    ],
+    image: '/assets/work/thumb3.png',
+    live: '',
+    github: '',
+  },
+];
 
 const Work = () => {
   return (
