084b2f099ee71b22053e080936e15281c0800e23
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-25 22:43:28 -0600

    Add style on the trigger the arrow

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -97,8 +97,8 @@ const Work = () => {
                 <Link href={project.live}>
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger>
-                        <BsArrowUpRight />
+                      <TooltipTrigger className='w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group'>
+                        <BsArrowUpRight className='text-white text-3xl group-hover:text-accent' />
                       </TooltipTrigger>
                       <TooltipContent>
                         <p>Live project</p>
