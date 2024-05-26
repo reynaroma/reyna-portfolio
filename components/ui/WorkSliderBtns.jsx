72fcf3f5ea5dc205f986a041eb86a17dfddc6389
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 17:52:41 -0600

    Add new file for swiper buttons and structure

---
+++ components/ui/WorkSliderBtns.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkSliderBtns = ({ containerStyles, btnStyles, iconStyles }) => {
+
+  const swiper = useSwiper();
+  return (
+    <div>WorkSliderBtns</div>
+  )
+}
+
+export default WorkSliderBtns
