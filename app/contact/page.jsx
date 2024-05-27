d63ab01369e95966523befeb3b8b71c741f882ea
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 22:20:52 -0600

    Add style on the info element

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -86,7 +86,23 @@ const Contact = () => {
             </form>
           </div>
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div>{item.icon}</div>
+                  </div>
+                  <div>
+                    <p>{item.title}</p>
+                    <h3>{item.description}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </motion.section>
