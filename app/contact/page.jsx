43ba065f73e48bb359394b55eaf24aa9701b58c8
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 20:32:27 -0600

    Add info object

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -15,6 +15,24 @@ import {
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
+const info = [ 
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '+1 306 684-0292'
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'roma.reynamay@gmail.com'
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: 'Address',
+    description: '1319 Warner Street, Moose Jaw, S6H 6K2, SK, Canada'
+  },
+];
+
 const Contact = () => {
   return (
     <div>Contact</div>
