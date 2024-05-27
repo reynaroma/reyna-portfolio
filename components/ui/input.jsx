d707cf03ed0cb13f5b3288c5dc0f2cf9f8548287
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 21:58:46 -0600

    Add border on the styles of input and textarea

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     (<input
       type={type}
       className={cn(
-        "flex h-[48px] rounded-md border-white/10 focus:border-accent outline-none font-light bg-primary px-4 py-5 text-base placeholder:text-white/60",
+        "flex h-[48px] rounded-md border border-white/10 focus:border-accent outline-none font-light bg-primary px-4 py-5 text-base placeholder:text-white/60",
         className
       )}
       ref={ref}
