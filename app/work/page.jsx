686755722917009ecdb8d33aff847c1cbb0cc5e1
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 20:07:22 -0600

    Add styles on the buttons of slider

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -157,7 +157,9 @@ const Work = () => {
                 </SwiperSlide>
               ))}
               {/* slider buttons */}
-              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles=""/>
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all" />
             </Swiper>
           </div>
         </div>
