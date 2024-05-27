972ebd6b627b862a101b783393103da4661b8dcc
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 20:30:05 -0600

    Import components needed for the work page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,23 @@
+"use client";
 
-function Contact() {
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const Contact = () => {
   return (
-    <div>
-      contact page
-    </div>
+    <div>Contact</div>
   )
 }
 
