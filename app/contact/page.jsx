70a32d0614f2caab96a970a2d5ad32da313e4968
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-05-26 20:49:23 -0600

    Add gap and bg color on the form element

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -48,7 +48,9 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
-            <form action="">form</form>
+            <form
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+              action="">form</form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
